d40c90bc78c4eb0e400a7a1ae262d7f3fadef628
CuzImBisonratte <cuzimbisonratte@gmail.com>
2023-01-24 23:05:57 +0100

    Style scrollNote onclick

--- res/js/scrollNote.js
+++ res/js/scrollNote.js
@@ -2,8 +2,10 @@ const scrollNote = document.getElementById("scrollNoteContainer");
 
 function scrollEvent() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollNote.style.cursor = "initial";
         scrollNote.style.opacity = "0";
     } else {
+        scrollNote.style.cursor = "pointer";
         scrollNote.style.opacity = "1";
     }
 }

a9dbad65ffbcbb9c14b61431ec2b7c20edbaa0e0
CuzImBisonratte <CuzImBisonratte@gmail.com>
2023-01-24 22:07:25 +0100

    Add scrollNote

---
+++ res/js/scrollNote.js
@@ -0,0 +1,12 @@
+const scrollNote = document.getElementById("scrollNoteContainer");
+
+function scrollEvent() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollNote.style.opacity = "0";
+    } else {
+        scrollNote.style.opacity = "1";
+    }
+}
+
+window.addEventListener("scroll", scrollEvent);
+scrollEvent();
